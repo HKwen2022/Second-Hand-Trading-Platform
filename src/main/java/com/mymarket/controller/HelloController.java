5a8c2f1c29906a23f9e1effb50dcf4f776f0a29a
Hkwen <3172534768@qq.com>
2023-07-21 16:59:00 +0800

    6

--- src/main/java/com/mymarket/controller/HelloController.java
+++ src/main/java/com/mymarket/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping("/")
     public String index(){
+        //跳转页面至用户登录界面
         return "forward:html/index.html";
     }
 }
