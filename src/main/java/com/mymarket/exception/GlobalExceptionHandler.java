809491a39c80fc3ae19c50abbee5c6fbe828a052
hou <1423223gg@gmail.com>
2023-07-30 12:11:26 +0800

    4

---
+++ src/main/java/com/mymarket/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.mymarket.exception;
+
+import com.mymarket.pojo.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Result ex(Exception ex){
+        ex.printStackTrace();
+        return Result.error("对不起，操作失败，请联系管理员");
+    }
+}
